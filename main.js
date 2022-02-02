9c3c7619169643b9db589c14fa263f4b237edf28
Michael Grumbine <mgrumbine3@gmail.com>
2022-02-01 16:36:39 -0800

    final commit

--- main.js
+++ main.js
@@ -8,6 +8,41 @@ var books = [
   }
 ];
 
+var fetch = function (query) {
+  $.ajax({
+    method: "GET",
+    url: "https://www.googleapis.com/books/v1/volumes?q=" + query,
+    dataType: "json",
+    success: function (data) {
+      addBooks(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    },
+  });
+};
+
+var addBooks = function (data) {
+  books = [];
+
+  for (var i = 0; i < data.items.length; i++) {
+    var bookData = data.items[i];
+
+    var book = {
+      title: bookData.volumeInfo.title || null,
+      author: bookData.volumeInfo.authors ? bookData.volumeInfo.authors[0] : null,
+      imageURL: bookData.volumeInfo.imageLinks ? bookData.volumeInfo.imageLinks.thumbnail : null,
+      pageCount: bookData.volumeInfo.pageCount || null,
+      isbn: bookData.volumeInfo.industryIdentifiers ?
+        bookData.volumeInfo.industryIdentifiers[0].identifier : null
+    };
+
+    books.push(book);
+  }
+
+  renderBooks();
+};
+
 var renderBooks = function () {
   var $booksDiv = $('.books');
   $booksDiv.empty();
@@ -16,10 +51,16 @@ var renderBooks = function () {
 
   for (var i = 0; i < books.length; i++) {
     var currentBook = books[i];
-    var completeBookTemplate = bookTemplate({title: currentBook.title, author: currentBook.author, pageCount: currentBook.pageCount, isbn: currentBook.isbn, imageURL: currentBook.imageURL});
+    var completeBookHTML = bookTemplate({title: currentBook.title, author: currentBook.author, pageCount: currentBook.pageCount, isbn: currentBook.isbn, imageURL: currentBook.imageURL});
 
-    $booksDiv.append(completeBookTemplate);
+    $booksDiv.append(completeBookHTML);
   }
 };
+
+$('.search').on('click', function () {
+  var search = $('#search-query').val();
+
+  fetch(search);
+});
 
 renderBooks();
